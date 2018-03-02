e981278903e2c5c8f5e34149d24d638d93c568cc
Paul Janiszewski <pauljaniszewski@Pauls-MacBook-Pro.local>
2018-03-02 15:16:19 -0500

    dsfs

--- scripts/server.js
+++ scripts/server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
+var http = require('http');
 
-app.get('/', function(req, res) {
+
+
+
+app.get('', function(req, res) {
 
 	res.send('HELLO CRYPTO WORLD!');
 });
 
 app.listen(3000, function(){
-
 	console.log('API NOW LISTENING ON PORT 3000');
 });
