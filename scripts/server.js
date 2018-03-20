3609f9ffae6060ae39aed3bfff35766c28232102
Paul Janiszewski <pauljaniszewski@Pauls-MacBook-Pro.local>
2018-03-19 22:16:46 -0400

    using express now

--- scripts/server.js
+++ scripts/server.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var app = express();
 var http = require('http');
+var router = express.Router();
 
 
 
 
-app.get('', function(req, res) {
-
-	res.send('HELLO CRYPTO WORLD!');
+router.get("/", function(req, res){
+	res.json
 });
+
+
+
+app.use("/api",router);
 
 app.listen(3000, function(){
 	console.log('API NOW LISTENING ON PORT 3000');
 });
+
