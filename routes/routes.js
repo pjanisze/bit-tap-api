0a5c64e5574b59ac066ced1fd2e05cded2199802
Paul Janiszewski <pauljaniszewski@Pauls-MacBook-Pro.local>
2018-05-12 13:40:09 -0400

    bugfix

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var request = require("request");
 var router = express.Router();
 var userService = require('../scripts/users');
-
+var ObjectId = require('mongodb').ObjectID;
 router.use(express.json());
 
 module.exports = function(app, db){
