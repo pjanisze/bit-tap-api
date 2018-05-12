f21d228ecf6e8268c769d2489d41bb689299bf81
Paul Janiszewski <pauljaniszewski@Pauls-MacBook-Pro.local>
2018-05-12 13:42:35 -0400

    bugfix

--- scripts/server.js
+++ scripts/server.js
@@ -6,7 +6,7 @@ var app = express();
 
 MongoClient.connect( "mongodb://localhost:27017/", ( err, db ) => {
 const database = db.db('bittapdb')
-require("../routes/routes") (app, db);
+require("../routes/routes") (app, database);
 
 app.listen(3000, function(){
 	console.log('API NOW LISTENING ON PORT 3000');
