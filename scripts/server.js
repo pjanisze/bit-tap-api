54796b146003f097a3074bc56d46a3f7fbc37309
Paul Janiszewski <pauljaniszewski@Pauls-MacBook-Pro.local>
2018-05-12 14:03:13 -0400

    bugfix

--- scripts/server.js
+++ scripts/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var MongoClient = require( 'mongodb' ).MongoClient;
 const bodyParser = require('body-parser');
 var app = express();
-
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 MongoClient.connect( "mongodb://localhost:27017/", ( err, db ) => {
 const database = db.db('bittapdb');
