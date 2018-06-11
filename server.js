bff2eb036478e5714b541e5fc536ee6784b35eb6
Paul Janiszewski <pauljaniszewski44@gmail.com>
2018-06-10 22:13:15 -0400

    Clean up unused files and add .env

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 
-	
+
 var express = require('express');
 var MongoClient = require( 'mongodb' ).MongoClient;
 const bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
-MongoClient.connect( "mongodb://localhost:27017/", ( err, db ) => {
+require('dotenv').load();
+
+
+const DB_URL = process.env.DB_URI|| '';
+const DB_PORT = process.env.DB_PORT|| '';
+const DB_URI = 'mongodb://' + DB_URL + ":" + DB_PORT + "/"
+
+MongoClient.connect(DB_URI, ( err, db ) => {
 const database = db.db('bittapdb');
-require("../routes/routes") (app, database);
+require("./routes/routes") (app, database);
 
 app.listen(3000, function(){
 	console.log('API NOW LISTENING ON PORT 3000');
